b9d4ad198ce697e4d4c1044bf7a28ba68f6b5fc7
gabescott10 <austin.g.scott-1@ou.edu>
2019-09-16 20:30:32 -0600

    trying to get project off github for xcode

--- project1.c
+++ project1.c
@@ -14,6 +14,59 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#define MAX_ARGS = 3;
+#define INBUFFSIZE = 128;   // size of buffer in bytes
 
 
-int main(argc, 
+const char SEPARATORS[] = " \t\n";
+char in_buffer[INBUFFSIZE];           // Input buffer from STDIN. command input buffer
+char * args[MAX_ARGS];               // pointers to arg strings
+char ** arg;                 // working pointer that steps through the args
+
+int list()
+{
+    
+}
+
+int zero()
+{
+
+}
+
+int writeByte(char* arg)   // set the specified byte to input value
+{
+    
+}
+
+int readByte(char* arg)
+{
+    
+}
+
+int writeHexByte(
+
+int main(int argc, const char * argv[])
+{
+    while(fgets(in_buffer, MAX_ARGS, stdin)!= NULL)
+    {
+        arg = args;
+        *arg++ = strtok(in_buffer,SEPARATORS);   // tokenize input  
+        while ((*arg++ = strtok(NULL,SEPARATORS)));
+        switch(arg[0])
+        {
+            case 'l': list(); 
+                      break;
+            case 'z': zero();
+                      break;
+            case 'b': writeByte(arg);
+                      break;
+            case 'B': readByte(arg);
+                      break;
+            case 'h': writeHexByte(arg);
+                        break;
+                      case 'H
+
+        }
+
+    return 0;
+}
